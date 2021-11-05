eec3aa95b1b8d8195ed531c02c129e46371b8803
Yannick B <yannickbiheul@gmail.com>
2021-11-05 12:09:01 +0100

    Modif commentaires truncateString

--- truncateString.js
+++ truncateString.js
@@ -4,14 +4,15 @@
 // Garder les premiers caractères dans une chaine "str", définis par un nombre "num", et retourner cette chaine non complète, en la terminant par 3 points (...).
 
 function truncateString(str, num) {
+    let result;
     // Transormer la chaine en tableau
     let strArray = str.split('');
-    let result;
     // Vérifier si "num" est égal ou supérieur à la taille de la chaine
     if (num >= strArray.length) {
+        // Si c'est le cas, on retourne la chaine complète
         result = str;
     } else {
-        // Récupérer les premiers éléments du tableau définis par "num"
+        // Sinon, on récupérer les premiers éléments du tableau définis par "num"
         let firstWords = strArray.splice(0, num);
         // Transformer le résultat en chaine de caractères et ajouter les 3 points
         result = firstWords.join('') + "...";
@@ -21,3 +22,4 @@ function truncateString(str, num) {
 }
 
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
+console.log(truncateString("Hello World", 4));
