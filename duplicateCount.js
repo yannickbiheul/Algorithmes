8a47738bc7ea026545ac98e402c1d419d89fe74d
Yannick B <yannickbiheul@gmail.com>
2021-11-13 21:03:26 +0100

    Tentative duplicateCount

--- duplicateCount.js
+++ duplicateCount.js
@@ -1,32 +1,29 @@
 // CodeWars
 
 // Compter le nombre d'éléments qui se répètent dans une chaine de caractères
+// Voir la méthode "indexOf" => https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 
 function duplicateCount(text) {
-    let result = [];
-    let array = text.toString().toLowerCase().split('');
-    let unique1;
-    let unique2;
+    let result;
 
-    for (let i = 0; i < array.length; i++) {
-        unique1 = "";
-        if (array[i] != unique1) {
-            for (let j = i + 1; j < array.length; j++) {
-                unique2 = "";
-                if (array[j] != unique2) {
-                    if (array[i] == array[j]) {
-                        result.push(array[i]);
-                        unique1 = array[i];
-                        unique2 = array[j];
-                    }
-                }
-                
-            }
-        }
-        
-    }
-
-    return result.length;
+    return result;
 }
 
-console.log(duplicateCount("Indivisibility"));
+let array = ["a", "b", "c", "d", "a", "b", "c", "d"];
+
+let count = 0;
+for (let i = 0; i < array.length; i++) {
+    let indices = [];
+    let element = array[i];
+    let idx = array.indexOf(element);
+    while (idx != -1) {
+        indices.push(idx);
+        idx = array.indexOf(element, idx + 1);
+    }
+    if (indices.length >= 1) {
+        count++;
+    }
+}
+
+console.log(count);
+
