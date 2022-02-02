650842347b5f2710dce367abe322ddbd3418c8cb
Yannick Biheul <yannickbiheul@outlook.fr>
2022-02-02 12:38:55 +0100

    Fonction affichage étoiles

--- Javascript_de_zero/triangleEtoiles.js
+++ Javascript_de_zero/triangleEtoiles.js
@@ -65,4 +65,115 @@ function afficherEtoilesDecroissantImpair(nombre) {
     }
 }
 
-afficherEtoilesDecroissantImpair(3);
+// afficherEtoilesDecroissantImpair(3);
+
+function etoilesCroissant(nombre) {
+    let ligne = "";
+    // nombre représente le nombre de lignes
+    for (let i = 0; i < nombre; i++) {
+        // On parcourt la ligne actuelle pour afficher les étoiles
+        for (let j = 0; j <= i; j++) {
+            ligne += "*";
+        }
+        // On affiche la ligne
+        console.log(ligne);
+        // On réinitialise la ligne
+        ligne = "";
+    }
+}
+
+function etoilesDecroissant(nombre) {
+    let ligne = "";
+    for (let i = nombre; i > 0; i--) {
+        for (let j = i; j > 0; j--) {
+            ligne += "*";
+        }
+        console.log(ligne);
+        ligne = "";
+    }
+}
+
+function etoiles(nombre) {
+    etoilesCroissant(nombre);
+    etoilesDecroissant(nombre - 1);
+}
+
+function etoilesCroissantPair(nombre) {
+    let ligne = "";
+    if (nombre % 2 === 0) {
+        for (let i = 2; i <= nombre; i += 2) {
+            for (let j = 0; j < i; j++) {
+                ligne += "*";
+            }
+            console.log(ligne);
+            ligne = "";
+        }
+    } else {
+        console.log("Ce nombre est impair !");
+    }
+}
+
+function etoilesDecroissantPair(nombre) {
+    let ligne = "";
+    if (nombre % 2 === 0) {
+        for (let i = nombre; i > 0; i -= 2) {
+            for (let j = i; j > 0; j--) {
+                ligne += "*";
+            }
+            console.log(ligne);
+            ligne = "";
+        }
+    } else {
+        console.log("Ce nombre est impair !");
+    }
+}
+
+function etoilesPair(nombre) {
+    if (nombre % 2 === 0) {
+        etoilesCroissantPair(nombre);
+        etoilesDecroissantPair(nombre - 2);
+    } else {
+        console.log("Ce nombre est impair !");
+    }
+}
+
+function etoilesCroissantImpair(nombre) {
+    if (nombre % 2 != 0) {
+        let ligne = "";
+        for (let i = 1; i <= nombre; i += 2) {
+            for (let j = 1; j <= i; j++) {
+                ligne += "*";
+            }
+            console.log(ligne);
+            ligne = "";
+        }
+    } else {
+        console.log("Ce nombre est pair !");
+    }
+}
+
+function etoilesDecroissantImpair(nombre) {
+    if (nombre % 2 != 0) {
+        let ligne = "";
+        for (let i = nombre; i >= 1; i -= 2) {
+            for (let j = i; j >= 1; j--) {
+                ligne += "*";
+            }
+            console.log(ligne);
+            ligne = "";
+        }
+    } else {
+        console.log("Ce nombre est pair !");
+    }
+}
+
+function etoilesImpair(nombre) {
+    if (nombre % 2 != 0) {
+        etoilesCroissantImpair(nombre);
+        etoilesDecroissantImpair(nombre - 2);
+    } else {
+        console.log("Ce nombre est pair !");
+    }
+}
+
+etoilesImpair(15);
