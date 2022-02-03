14511e4a07e169d8b26f5e783496967a268b7802
Yannick Biheul <yannickbiheul@outlook.fr>
2022-02-03 18:45:27 +0100

    Ajout de pillars.js

---
+++ CodeWars/evenOrOdd.js
@@ -0,0 +1,7 @@
+function even_or_odds(number) {
+    if (number % 2 === 0) {
+        return 'Even';
+    } else {
+        return 'Odd';
+    }
+}
---
+++ CodeWars/pillars.js
@@ -0,0 +1,10 @@
+function pillars(num_pill, dist, width) {
+    let result = 0;
+    if (num_pill <= 1) {
+        result = 0;
+    }
+    
+    return result;
+}
+
+console.log(pillars(1, 10, 10));
