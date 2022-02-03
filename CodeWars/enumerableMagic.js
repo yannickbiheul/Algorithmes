2d74b4c027f16142d7746917e3b0b121f9af875d
Yannick Biheul <yannickbiheul@outlook.fr>
2022-02-03 18:14:51 +0100

    Résolution de enumerableMagic !

--- CodeWars/enumerableMagic.js
+++ CodeWars/enumerableMagic.js
@@ -3,23 +3,15 @@ function eachCons(array, n) {
     let tableau1 = [];
     let tableau2 = [];
     for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < n; j++) {
-            if (array[i + j]) {
-                tableau2.push(array[i + j]);
-            } else {
-                break;
-            } 
+        tableau2 = array.slice(i, n + i);
+        if (tableau2.length < n) {
+            break;
         }
         tableau1.push(tableau2);
-        tableau2 = [];
-    }
-    // console.log(tableau1[tableau1.length - 1][0]);
-    // console.log(array[array.length - 1]);
-    if (tableau1[tableau1.length - 1][0] === array[array.length - 1]) {
-        tableau1.pop();
     }
     
     return(tableau1);
 }
 
-console.log(eachCons([3,5,8,13], 1));
+console.log(eachCons([3,5,8,13], 2));
+
