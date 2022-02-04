656fc14b0a86b5bc74ab2813d4a4dbd301eb58d9
Yannick Biheul <yannickbiheul@outlook.fr>
2022-02-04 10:24:28 +0100

    Résolution de pillars

--- CodeWars/pillars.js
+++ CodeWars/pillars.js
@@ -1,10 +1,18 @@
 function pillars(num_pill, dist, width) {
-    let result = 0;
-    if (num_pill <= 1) {
-        result = 0;
+    if (num_pill < 2) {
+        return 0;
     }
-    
-    return result;
+    let distCM = dist * 100;
+    // 30 + 1500 = 1530
+    let largeur = width + distCM;
+    // 1530 * 11 = 16830
+    let total = largeur * num_pill;
+    // 16830 - 1500 = 15330
+    total -= distCM;
+    // 15330 - (30 * 2) = 15270
+    total -= width * 2;
+
+    return total;
 }
 
-console.log(pillars(1, 10, 10));
+console.log(pillars(2,20,25));
